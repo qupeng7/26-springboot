a53f7577a456e516ca42a4089b7ab69aaed456aa
qupeng <qupeng7@126.com>
2019-11-08 20:42:29 +0800

    commit

--- src/main/java/com/qupeng/springboot/Application.java
+++ src/main/java/com/qupeng/springboot/Application.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages={"com.qupeng.springboot.mapper"})//扫描包之后,mapper注解可以去掉
+//@MapperScan(basePackages={"com.qupeng.springboot.mapper"})//扫描包之后,mapper注解可以去掉
 @SpringBootApplication
 public class Application {
 
